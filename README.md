511d6a8407d122de04a4cb8baccc0d7697002eac
Andrei Muntean <andrei.muntean.dev@gmail.com>
2015-08-23 06:30:50 +0300

    Updated readme and image formats

--- README.md
+++ README.md
@@ -3,11 +3,20 @@
 
 The official CSSoc website.
 
-###Acceptable CSS Units of Measurement
+## Design Documentation
+### Acceptable CSS Units of Measurement
   * px
     * Pixels (px) are relative to the viewing device. For low-dpi devices, 1px is one device pixel (dot) of the display. For printers and high resolution screens 1px implies multiple device pixels.
   * %
   * vw, vh
     * For desktop only.
     * Always include px as well (to support ancient browsers) like so:
-      * height: 75px; height: 8vh;
+      * `height: 75px; height: 8vh`;
+
+### Image Sizes
+  * Maximum height of sponsor logo: **140px**.
+  * Social media icons: **60px × 60px**.
+  * Header icon: **50px × 50px**.
+
+## License
+MIT © [CSSoc](https://github.com/cssoc)
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,76 @@
 var gulp = require('gulp');
+var imageResize = require('gulp-image-resize');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 
 gulp.task('minify-css', function() {
-    return gulp.src(['assets/css/*.css', '!assets/css/*.min.*'])
+    gulp.src(['assets/css/*.css', '!assets/css/*.min.*'])
         .pipe(minifyCss())
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(gulp.dest('assets/css'));
 });
+
+gulp.task('create-small-committee-images', function() {
+    gulp.src(['assets/images/committee/*.*', '!assets/images/committee/*.*.*'])
+        .pipe(imageResize({
+            imageMagick: true,
+            width: 200,
+            height: 200,
+            crop: true,
+            upscale: true,
+            format: 'jpeg',
+            quality: 0.8,
+            filter: 'Catrom'
+        }))
+        .pipe(rename({
+            suffix: '.small',
+            extname: '.jpg'
+        }))
+        .pipe(gulp.dest('assets/images/committee'));
+});
+
+gulp.task('create-small-gallery-images', function() {
+    gulp.src(['assets/images/gallery/**/*.*', '!assets/images/gallery/**/*.*.*'])
+        .pipe(imageResize({
+            imageMagick: true,
+            width: 150,
+            height: 150,
+            crop: true,
+            upscale: true,
+            format: 'jpeg',
+            quality: 0.8,
+            filter: 'Catrom'
+        }))
+        .pipe(rename({
+            suffix: '.small',
+            extname: '.jpg'
+        }))
+        .pipe(gulp.dest('assets/images/gallery'));
+});
+
+gulp.task('create-large-gallery-images', function() {
+    gulp.src(['assets/images/gallery/**/*.*', '!assets/images/gallery/**/*.*.*'])
+        .pipe(imageResize({
+            imageMagick: true,
+            height: 720,
+            upscale: true,
+            format: 'jpeg',
+            quality: 0.8,
+            filter: 'Catrom'
+        }))
+        .pipe(rename({
+            suffix: '.large',
+            extname: '.jpg'
+        }))
+        .pipe(gulp.dest('assets/images/gallery'));
+});
+
+gulp.task('convert-images', [
+	'create-small-committee-images',
+	'create-small-gallery-images',
+	'create-large-gallery-images'
+]);
 
 gulp.task('default', ['minify-css']);
