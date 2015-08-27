021628ea7faaf3a99e11476583f8c622728e4252
Andrei Muntean <andrei.muntean.dev@gmail.com>
2015-08-27 05:17:06 +0300

    Multiple minor UI tweaks

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,8 +1,14 @@
+var desktopPhoto = document.getElementById('desktop-photo');
+var mobilePhoto = document.getElementById('mobile-photo');
+
 function selectPhoto(albumId, photoId) {
     var photoPath = '/assets/images/gallery/' + albumId + '/' + photoId + '.large.jpg';
-    var desktopPhoto = document.getElementById('desktop-photo');
-    var mobilePhoto = document.getElementById('mobile-photo');
 
+    // Clears the displayed photo to indicate that it is being updated.
+    desktopPhoto.src = '';
+    mobilePhoto.src = '';
+
+    // Starts loading the selected photo.
     desktopPhoto.src = photoPath;
     mobilePhoto.src = photoPath;
 }
